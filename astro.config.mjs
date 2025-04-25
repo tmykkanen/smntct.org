a09d5bbf319fe60f6fb27c51d66074d9acdb0656
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-04-25 14:15:06 -0500

    more initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [icon()],
+
+  site: 'https://smntct.org'
 });
