37afc1d2eb9641c05d26a84e2ad24d1c6f2ca49f
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-07-04 08:46:38 -0500

    fix: :bug: fix resize bugs + implement dynamic text resize

--- src/entrypoints/alpine.ts
+++ src/entrypoints/alpine.ts
@@ -1,7 +1,9 @@
 import type { Alpine } from "alpinejs";
 import intersect from "@alpinejs/intersect";
+import resize from "@alpinejs/resize";
 
 export default (Alpine: Alpine) => {
   Alpine.plugin(intersect);
+  Alpine.plugin(resize);
   Alpine.store("menu", false);
 };
